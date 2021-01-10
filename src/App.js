f55e9f5bd9087019305df5d67a6429b135644eb0
jadu20 <audu1217@naver.com>
2021-01-10 17:56:18 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
+import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
 
-function Food({ name }){
-  return <h1>I like {name}</h1>;
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  };
+
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
+  };
+  componentDidMount = () => {
+    this.getMovies();
+  };
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+    );
+  }
 }
-
-const foodILike = [{
-  id:1,
-  name:"Bibimbap"
-},
-{
-  id:2,
-  name:"kimbap"
-}
-]
-
-function App() {
-  return (
-    <div>  
-      <h1>Hello</h1>
-      {foodILike.map(dish => <Food key={dish.id} name={dish.name}/>)}
-    </div>
-  );
-} 
 
 export default App;
